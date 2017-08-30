cc0333b396ec40d95204a958599dae4bff5235a7
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-08-30 15:58:48 -0500

    Adds route and credentials for accessing API

--- app.js
+++ app.js
@@ -3,12 +3,33 @@ const app = express();
 const path = require('path');
 
 const PORT = 8000;
-// const KEY = require(API KEY HERE)
+
+const KEY = require('./spotify_tokens.json');
+
 
 app.use('/', express.static(__dirname + '/public'));
 
+
 app.get('/', function(req, res) {
     res.sendFile('index.html');
+});
+
+app.get('/artist', function (req, res) {
+  var spotifyQueryRequest;
+  spotifyQueryString = $('#spotify-q').val();
+  // searchUrl = "https://api.spotify.com/v1/search?type=artist&q=" + spotifyQueryString;
+  searchUrl = "https://api.spotify.com/v1/search?type=artist&q=madona";
+
+  // Generate the request object
+  spotifyQueryRequest = $.ajax({
+      type: "GET",
+      dataType: 'json',
+      url: searchUrl
+  });
+
+  spotifyQueryRequest.done(function (data) {
+    res.send();
+  });
 });
 
 app.listen(PORT, function() {
